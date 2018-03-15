18d0401e0b115597fb26dfe7a93651045eee7a90
Ajani Stewart <theamjaha@gmail.com>
2018-03-15 10:50:59 -0400

    lab_06

--- lab_03-2/funcs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <fstream>
-
-double east_storage(const std::string &dateIn);
-double mineast();
-double maxeast();
-std::string compare(const std::string &dateStart, const std::string &dateEnd);
-std::string reverse_order(const std::string &dateStart, const std::string &dateEnd);
